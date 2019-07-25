5947c5132d5fc036fea4797ece7911ec79891ae5
jaegerj <you@example.com>
2019-07-25 17:13:37 -0400

    Generic CRUD para Reviews, falta probar

--- course/views.py
+++ course/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from . import models
 from . import serializer
@@ -10,3 +11,27 @@ class ListCreateCourses(generics.ListCreateAPIView):
 class RetrieveUpdateDestroyCourses(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Courses.objects.all()
     serializer_class = serializer.CoursesSerializer
+
+class ListCreateReview(generics.ListCreateAPIView):
+    queryset = models.Review.objects.all()
+    serializer_class = serializer.ReviewSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(course_id=self.kwargs.get('course_pk'))
+
+    def perform_create(self, serializer):
+        course = get_object_or_404(
+            models.Courses, pk=self.kwargs.get('course_pk'))
+        serializer.save(course=course)
+    
+
+class RetrieveUpdateDestroyReview(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Review.objects.all()
+    serializer_class = serializer.ReviewSerializer
+
+    def get_object(self):
+        return get_object_or_404(
+            self.get_queryset(),
+            course_id=self.kwargs.get('course_pk'),
+            pk=self.kwargs.get('pk')
+        )
