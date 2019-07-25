b2ab8f4e15ef795c42d73d6a55550b39f837ff3b
jaegerj <you@example.com>
2019-07-25 15:46:32 -0400

    Utilizando Generics Views del DRF)

--- course/urls.py
+++ course/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.ListCreateCourses.as_view(), name = 'course_list')
+    url(r'^$', views.ListCreateCourses.as_view(), name = 'course_list'),
+    url(r'(?P<pk>\d+)/$', views.RetrieveUpdateDestroyCourses.as_view(), name = 'course_detail')
 ]
